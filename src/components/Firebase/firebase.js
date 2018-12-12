9a354a12aab963b4d510c5efa9541c884b2341f5
Isaac <hawkins.isaac@gmail.com>
2018-12-11 21:48:12 -0600

    create firebase auth methods

--- src/components/Firebase/firebase.js
+++ src/components/Firebase/firebase.js
@@ -12,6 +12,26 @@ var config = {
 class Firebase {
   constructor() {
     app.initializeApp(config);
+
+    this.auth = app.auth();
+  }
+
+  // AUTH API //
+
+  doCreateUserWithEmailAndPassword = (email, password) => {
+    this.auth.createUserWithEmailAndPassword(email,password);
+  }
+
+  doSignInWithEmailAndPassword = (email, password) => {
+    this.auth.signInWithEmailAndPassword(email,password);
+  }
+
+  doSignout = () => this.auth.signOut();
+
+  doPasswordReset = email => this.auth.sendPasswordResetEmail(email);
+
+  doPasswordUpdate = password => {
+    this.auth.currentUser.updatePassword(password);
   }
 }
 
