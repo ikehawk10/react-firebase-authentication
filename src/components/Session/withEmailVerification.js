7c0859aa9f0023e7b3f31e9603e0d52114835a34
Isaac <hawkins.isaac@gmail.com>
2018-12-13 17:44:43 -0600

    create messages and email notifications

---
+++ src/components/Session/withEmailVerification.js
@@ -0,0 +1,67 @@
+import React from 'react';
+
+import AuthUserContext from './context';
+import { withFirebase } from '../Firebase';
+
+const needsEmailVerification = authUser =>
+  authUser &&
+  !authUser.emailVerified &&
+  authUser.providerData
+    .map(provider => provider.providerId)
+    .includes('password');
+
+const withEmailVerification = Component => {
+  class WithEmailVerification extends React.Component {
+    constructor(props) {
+      super(props);
+
+      this.state = { isSent: false };
+    }
+
+    onSendEmailVerification = () => {
+      this.props.firebase
+        .doSendEmailVerification()
+        .then(() => this.setState({ isSent: true }));
+    };
+
+    render() {
+      return (
+        <AuthUserContext.Consumer>
+          {authUser =>
+            needsEmailVerification(authUser) ? (
+              <div>
+                {this.state.isSent ? (
+                  <p>
+                    E-mail confirmation sent: Check your e-mail (Spam
+                    folder included) for a confirmation e-mail.
+                    Refresh this page once you confirmed your e-mail.
+                  </p>
+                ) : (
+                  <p>
+                    Verify your e-mail: Check you e-mails (Spam folder
+                    included) for a confirmation e-mail or send
+                    another confirmation e-mail.
+                  </p>
+                )}
+
+                <button
+                  type="button"
+                  onClick={this.onSendEmailVerification}
+                  disabled={this.state.isSent}
+                >
+                  Send confirmation e-mail
+                </button>
+              </div>
+            ) : (
+              <Component {...this.props} />
+            )
+          }
+        </AuthUserContext.Consumer>
+      );
+    }
+  }
+
+  return withFirebase(WithEmailVerification);
+};
+
+export default withEmailVerification;
